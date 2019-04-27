416c55245500ebd3b506fb4b2f3549d83762254b
Derek Townsend <derekjavon2@gmail.com>
2019-04-27 16:45:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,18 +4,19 @@ grandma_statement="HUH?! SPEAK UP, SONNY!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if (statement==statement.upcase)
-  if(statement=="I LOVE YOU GRANDMA!")
+  if (statement==statement.upcase)
+    if(statement=="I LOVE YOU GRANDMA!")
+
+    end
+
+  # If you shout, she can hear you (or at least she thinks so)
+  # and yells back
+
+  # NO, NOT SINCE 1938!
+
+  # However if you say 'I LOVE YOU GRANDMA!', she should respond with
+  # 'I LOVE YOU TOO PUMPKIN!'
 
   end
-
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
-
-
+  return grandma_statement
 end
